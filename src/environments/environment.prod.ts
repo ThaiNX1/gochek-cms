7004393d7f56557f63cd757afdceb8d0671f2882
ThaiNX1 <nxthai22@gmail.com>
2025-12-10 10:57:51 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiGraphQL: 'https://gocheckbe-production.up.railway.app/graphql',
-  socket: 'https://gocheckbe-production.up.railway.app/graphql',
-  apiRestFull: 'https://gocheckbe-production.up.railway.app',
+  apiGraphQL: 'https://gochekbe-production.up.railway.app/graphql',
+  socket: 'https://gochekbe-production.up.railway.app/graphql',
+  apiRestFull: 'https://gochekbe-production.up.railway.app',
   // keymap: 'AIzaSyAY5uooPOZnlgZ2hHjM3zqUj3q0PRqaky8',
   keymap: 'AIzaSyBXt-XpsrduzVKg1RSjuk5whVe9jfzALQQ',
 };
