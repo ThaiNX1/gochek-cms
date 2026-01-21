bfa2dcb22d00ab308b940ca65823b6fc9e116f0d
ThaiNX1 <nxthai22@gmail.com>
2026-01-21 17:31:40 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiGraphQL: 'https://gochek-be.tmtshop.io.vn/graphql',
-  socket: 'https://gochek-be.tmtshop.io.vn/graphql',
-  apiRestFull: 'https://gochek-be.tmtshop.io.vn',
+  apiGraphQL: 'https://stg-api.denden.vn/graphql',
+  socket: 'https://stg-api.denden.vn/graphql',
+  apiRestFull: 'https://stg-api.denden.vn',
   // keymap: 'AIzaSyAY5uooPOZnlgZ2hHjM3zqUj3q0PRqaky8',
   keymap: 'AIzaSyBXt-XpsrduzVKg1RSjuk5whVe9jfzALQQ',
 };
